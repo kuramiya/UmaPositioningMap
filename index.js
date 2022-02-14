852fbf62f01815227c430040cf98bba27df19cf5
kuramiya <s_kuramiya@yahoo.co.jp>
2022-02-14 16:24:52 +0900

    スマホ向けの操作性設定を追加

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 
 interact('.draggable').draggable({
+  
+  // enable inertial throwing
+  inertia: true,  
+
   listeners: {
     // start (event) {
     //   console.log(event.type, event.target)
     // },
     move (event) {
-      var target = event.target
+      var target = event.target;
+
       // keep the dragged position in the data-x/data-y attributes
-      var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-      var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+      var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+      var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
     
       // translate the element
-      target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+      target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
     
       // update the posiion attributes
-      target.setAttribute('data-x', x)
-      target.setAttribute('data-y', y)    },
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+    },
   }
 });
 
